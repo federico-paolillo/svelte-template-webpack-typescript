c7aa1173554030a16b1686150c0326b1d4801782
Federico Paolillo <federico.paolillo@triman.ch>
2022-02-05 22:14:11 +0100

    Template without HMR

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+/// <reference types="svelte" />
+
+import App from "@svelte-app/App.svelte";
+
+const app = new App({
+  target: document.body,
+  props: {
+    name: "world",
+  },
+});
+
+export default app;
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const sveltePreprocess = require("svelte-preprocess");
+const DotenvWebpackPlugin = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const PathsPlugin = require("tsconfig-paths-webpack-plugin").default;
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const prod = mode === "production";
+
+const SRC_FOLDER = path.resolve(__dirname, "src/");
+const DIST_FOLDER = path.resolve(__dirname, "dist/");
+const ASSETS_FOLDER = path.resolve(__dirname, "public/");
+
+const ENTRY = path.resolve(SRC_FOLDER, "main.ts");
+
+module.exports = {
+  entry: ENTRY,
+  resolve: {
+    alias: {
+      svelte: path.dirname(require.resolve("svelte/package.json")),
+    },
+    extensions: [".mjs", ".js", ".ts", ".svelte"],
+    mainFields: ["svelte", "browser", "module", "main"],
+    plugins: [
+      new PathsPlugin({
+        extensions: [".mjs", ".js", ".ts", ".svelte"],
+      }),
+    ],
+  },
+  output: {
+    path: DIST_FOLDER,
+    filename: "[contenthash].[name].js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svelte$/,
+        loader: "svelte-loader",
+        options: {
+          compilerOptions: {
+            dev: true,
+          },
+          emitCss: true,
+          preprocess: sveltePreprocess({
+            tsconfigFile: "tsconfig.json",
+            sourceMap: true,
+          }),
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        //Required to prevent errors from Svelte on Webpack 5+
+        test: /node_modules\/svelte\/.*\.mjs$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  optimization: {
+    moduleIds: "deterministic",
+    runtimeChunk: "single",
+  },
+  devServer: {
+    client: {
+      logging: "info",
+      overlay: true,
+      progress: true,
+    },
+    static: {
+      directory: ASSETS_FOLDER,
+      publicPath: "/public/",
+      watch: true,
+      serveIndex: false,
+    },
+    devMiddleware: {
+      index: "index.html",
+      publicPath: "/",
+    },
+    compress: false,
+    port: 65535,
+    open: false,
+    historyApiFallback: true,
+    https: false,
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[contenthash].[name].css",
+    }),
+    new DotenvWebpackPlugin({
+      allowEmptyValues: false,
+      safe: true,
+      systemvars: false,
+      silent: false,
+      expand: false,
+      ignoreStub: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+      title: "Svelte App",
+    }),
+    new ESLintPlugin({
+      failOnError: true,
+      failOnWarning: false,
+      emitError: true,
+      emitWarning: true,
+    }),
+  ],
+  devtool: prod ? false : "eval-source-map",
+};
